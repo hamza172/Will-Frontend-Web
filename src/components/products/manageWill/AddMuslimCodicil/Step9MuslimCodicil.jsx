2ec1a6b4ec1590a62ac198b12eebfea821bafddd
CharsiJawaan <shakeelusama3@gmail.com>
2021-09-12 15:17:29 -0700

    tracking all files

---
+++ src/components/products/manageWill/AddMuslimCodicil/Step9MuslimCodicil.jsx
@@ -0,0 +1,222 @@
+import React, { useState } from "react";
+import FormContainer from "../../willcreation/FormContainer";
+import { Form } from "react-bootstrap";
+
+import Button from "@material-ui/core/Button";
+import RemoveIcon from "@material-ui/icons/Remove";
+import AddIcon from "@material-ui/icons/Add";
+
+const Step9MuslimCodicil = ({
+  values,
+  nextStep,
+  prevStep,
+  handleChange,
+  changeState,
+  updateAndClose,
+  onFileChange,
+}) => {
+  const Continue = (e) => {
+    e.preventDefault();
+    nextStep();
+  };
+
+  const Previous = (e) => {
+    e.preventDefault();
+    prevStep();
+  };
+
+  return (
+    <FormContainer>
+      <Form.Label
+        as="legend"
+        className="text-center mb-5"
+        style={{ backgroundColor: "beige" }}
+      >
+        Only ONE-THIRD of the total asset can be distributed for Charitable
+        Giving and Distributions.
+      </Form.Label>
+      <h3>Step 9: Distribution Details</h3>
+      <Form>
+        {[...Array(values.step7AssetDetails?.length)].map((e, i) => (
+          <div key={i + 1}>
+            <h5>
+              Add Beneficiaries For Asset:{" "}
+              {values.step7AssetDetails[i].assetType}
+            </h5>
+            {[...Array(values.step7AssetDetails[i].beneficiaries.length)].map(
+              (e, j) => (
+                <div key={j + 1}>
+                  <Form.Group controlId="beneficiary">
+                    <Form.Label>Beneficiary</Form.Label>
+                    <Form.Control
+                      as="select"
+                      value={values.step7AssetDetails[i].beneficiaries[j].type}
+                      onChange={(e) => {
+                        values.step7AssetDetails[i].beneficiaries[j].type =
+                          e.target.value;
+                        changeState(
+                          values.step7AssetDetails[i].beneficiaries[j].type
+                        );
+                      }}
+                    >
+                      <option selected disabled value="">
+                        [Please select one]
+                      </option>
+                      <option value="Sister">Sister</option>
+                      <option value="Friends">Friends</option>
+                      <option value="Family">Family Member</option>
+                      <option value="Pet">Pet</option>
+                      <option value="Others">Others</option>
+                    </Form.Control>
+                  </Form.Group>
+
+                  {values.step7AssetDetails[i].beneficiaries[j].type !==
+                    "Wife" &&
+                    values.step7AssetDetails[i].beneficiaries[j].type !==
+                      "Child" && (
+                      <>
+                        <Form.Group controlId="name">
+                          <Form.Label>Full Name of Beneficiary</Form.Label>
+                          <Form.Control
+                            type="text"
+                            value={
+                              values.step7AssetDetails[i].beneficiaries[j].name
+                            }
+                            onChange={(e) => {
+                              values.step7AssetDetails[i].beneficiaries[
+                                j
+                              ].name = e.target.value;
+                              changeState(
+                                values.step7AssetDetails[i].beneficiaries[j]
+                                  .name
+                              );
+                            }}
+                          ></Form.Control>
+                        </Form.Group>
+
+                        <Form.Group controlId="address">
+                          <Form.Label>Address</Form.Label>
+                          <Form.Control
+                            type="text"
+                            value={
+                              values.step7AssetDetails[i].beneficiaries[j]
+                                .address
+                            }
+                            onChange={(e) => {
+                              values.step7AssetDetails[i].beneficiaries[
+                                j
+                              ].address = e.target.value;
+                              changeState(
+                                values.step7AssetDetails[i].beneficiaries[j]
+                                  .address
+                              );
+                            }}
+                          ></Form.Control>
+                        </Form.Group>
+
+                        <Form.Group controlId="email">
+                          <Form.Label>Email</Form.Label>
+                          <Form.Control
+                            type="text"
+                            value={
+                              values.step7AssetDetails[i].beneficiaries[j].email
+                            }
+                            onChange={(e) => {
+                              values.step7AssetDetails[i].beneficiaries[
+                                j
+                              ].email = e.target.value;
+                              changeState(
+                                values.step7AssetDetails[i].beneficiaries[j]
+                                  .email
+                              );
+                            }}
+                          ></Form.Control>
+                        </Form.Group>
+
+                        <Form.Group controlId="ph">
+                          <Form.Label>Phone Number</Form.Label>
+                          <Form.Control
+                            type="number"
+                            name="ph"
+                            value={
+                              values.step7AssetDetails[i].beneficiaries[j]
+                                .phoneNumber
+                            }
+                            onChange={(e) => {
+                              values.step7AssetDetails[i].beneficiaries[
+                                j
+                              ].phoneNumber = e.target.value;
+                              changeState(
+                                values.step7AssetDetails[i].beneficiaries[j]
+                                  .phoneNumber
+                              );
+                            }}
+                          ></Form.Control>
+                        </Form.Group>
+                      </>
+                    )}
+                </div>
+              )
+            )}
+            <Button
+              className="mr-3"
+              color="primary"
+              variant="contained"
+              disabled={values.step7AssetDetails[i].beneficiaries.length === 1}
+              onClick={(e) => {
+                e.preventDefault();
+                values.step7AssetDetails[i].beneficiaries.pop();
+                changeState(values.step7AssetDetails[i].beneficiaries);
+              }}
+            >
+              Delete <RemoveIcon />
+            </Button>
+
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={(e) => {
+                e.preventDefault();
+                values.step7AssetDetails[i].beneficiaries.push({
+                  type: "",
+                  name: "",
+                  address: "",
+                  email: "",
+                  phoneNumber: "",
+                });
+                changeState(values.step7AssetDetails[i].beneficiaries);
+              }}
+            >
+              Add More <AddIcon />
+            </Button>
+
+            <hr
+              style={{
+                marginBottom: "3rem",
+                marginTop: "3rem",
+                border: "1px solid #000",
+              }}
+            ></hr>
+          </div>
+        ))}
+      </Form>
+      <button className="btn btn-primary" onClick={Previous}>
+        Prev
+      </button>
+      <button className="btn btn-primary" onClick={Continue}>
+        Next
+      </button>
+      <br></br>
+      <button
+        className="btn btn-primary"
+        onClick={(e) => {
+          updateAndClose(e);
+        }}
+      >
+        Update & Close
+      </button>
+    </FormContainer>
+  );
+};
+
+export default Step9MuslimCodicil;
---
+++ src/components/products/willcreation/MuslimWillCreation/Step3Muslim.jsx
@@ -0,0 +1,187 @@
+import React, { useState } from "react";
+import FormContainer from "../FormContainer";
+import { Form } from "react-bootstrap";
+import Button from "@material-ui/core/Button";
+import RemoveIcon from "@material-ui/icons/Remove";
+import AddIcon from "@material-ui/icons/Add";
+
+const Step3Muslim = ({
+  nextStep,
+  prevStep,
+  handleChange,
+  changeState,
+  values,
+}) => {
+  const Continue = (e) => {
+    e.preventDefault();
+    nextStep();
+  };
+
+  const Previous = (e) => {
+    e.preventDefault();
+    prevStep();
+  };
+
+  return (
+    <FormContainer>
+      <Form.Label
+        as="legend"
+        className="text-center mb-5"
+        style={{ backgroundColor: "beige" }}
+      >
+        Add family members and your relationship with them. Family members must
+        include Husbands/Wives/Spouses, Sons, Daughters, Sister, Brothers, Grand
+        Father, Grand Mother, Uncles that is entitled to your estate according
+        to Islamic law. Please note Will be shared to your family according to
+        Islamic right. Please click the{" "}
+        <span style={{ cursor: "pointer", color: "blue" }}>“Schedule A”</span>{" "}
+        to understand Islamic sharing formula.
+      </Form.Label>
+      <h4 className="mb-5"> Step 3: Wives and Family Members for Wasiyah</h4>
+
+      <Form>
+        {[...Array(values.wivesDetails.length)].map((e, i) => (
+          <div key={i + 1}>
+            <Form.Group controlId="name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                name="name"
+                value={values.wivesDetails[i].name}
+                onChange={(e) => {
+                  values.wivesDetails[i].name = e.target.value;
+                  changeState(values.wivesDetails[i].name);
+                }}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="dob">
+              <Form.Label>Date Of Birth</Form.Label>
+              <Form.Control
+                type="date"
+                name="dob"
+                value={values.wivesDetails[i].dob}
+                onChange={(e) => {
+                  values.wivesDetails[i].dob = e.target.value;
+                  changeState(values.wivesDetails[i].dob);
+                }}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="city">
+              <Form.Label>City</Form.Label>
+              <Form.Control
+                type="text"
+                name="city"
+                value={values.wivesDetails[i].city}
+                onChange={(e) => {
+                  values.wivesDetails[i].city = e.target.value;
+                  changeState(values.wivesDetails[i].city);
+                }}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="zipcode">
+              <Form.Label>Zip Code</Form.Label>
+              <Form.Control
+                type="text"
+                name="zipCode"
+                value={values.wivesDetails[i].zipCode}
+                onChange={(e) => {
+                  values.wivesDetails[i].zipCode = e.target.value;
+                  changeState(values.wivesDetails[i].zipCode);
+                }}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="state">
+              <Form.Label>State</Form.Label>
+              <Form.Control
+                type="text"
+                name="state"
+                value={values.wivesDetails[i].state}
+                onChange={(e) => {
+                  values.wivesDetails[i].state = e.target.value;
+                  changeState(values.wivesDetails[i].state);
+                }}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="address">
+              <Form.Label>Address</Form.Label>
+              <Form.Control
+                type="text"
+                name="address"
+                value={values.wivesDetails[i].address}
+                onChange={(e) => {
+                  values.wivesDetails[i].address = e.target.value;
+                  changeState(values.wivesDetails[i].address);
+                }}
+              ></Form.Control>
+            </Form.Group>
+
+            <Button
+              className="mr-3"
+              color="primary"
+              variant="contained"
+              disabled={values.wivesDetails.length === 1}
+              onClick={(e) => {
+                var temp = [...values.wivesDetails];
+                var foo = -1;
+                for (var j = 0; j < values.wivesDetails.length; j++) {
+                  if (i + 1 === values.wivesDetails[j].index) {
+                    foo = i;
+                    break;
+                  }
+                }
+                temp.splice(foo, 1);
+                changeState("wivesDetails", temp);
+              }}
+            >
+              Delete
+              <RemoveIcon />
+            </Button>
+
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={(e) => {
+                e.preventDefault();
+                var temp = [...values.wivesDetails];
+                temp.push({
+                  index: values.wivesDetails.length + 1,
+                  name: "",
+                  dob: "",
+                  city: "",
+                  zipCode: "",
+                  state: "",
+                  address: "",
+                });
+                changeState("wivesDetails", temp);
+              }}
+            >
+              Add More
+              <AddIcon />
+            </Button>
+
+            <hr
+              style={{
+                marginBottom: "3rem",
+                marginTop: "3rem",
+                border: "1px solid #000",
+              }}
+            ></hr>
+          </div>
+        ))}
+        <button className="btn btn-primary" onClick={Previous}>
+          Prev
+        </button>
+        <button className="btn btn-primary" onClick={Continue}>
+          Next
+        </button>
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default Step3Muslim;
