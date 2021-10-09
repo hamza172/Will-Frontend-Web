0a6af33805abdfe3515ceae7c5d3cf7bcaa4e4e5
hamza172 <hamzammuazzam172@gmail.com>
2021-10-09 19:40:00 +0500

    Done

--- src/components/landingPage.jsx
+++ src/components/landingPage.jsx
@@ -210,17 +210,17 @@ class LandingPage extends Component {
     </div>
 
 
-    <footer class="site-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-12 col-md-6">
+    <footer className="site-footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 col-md-6">
             <h6>About</h6>
-            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ante nibh, elementum et magna in, finibus convallis purus. Suspendisse mattis tincidunt luctus. Cras quis placerat sapien. Praesent semper vehicula lacinia. Nunc ac cursus ipsum, et molestie tortor. Morbi nec sem lacus. Mauris auctor sed augue vel lacinia. Sed sed dictum metus.</p>
+            <p className="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ante nibh, elementum et magna in, finibus convallis purus. Suspendisse mattis tincidunt luctus. Cras quis placerat sapien. Praesent semper vehicula lacinia. Nunc ac cursus ipsum, et molestie tortor. Morbi nec sem lacus. Mauris auctor sed augue vel lacinia. Sed sed dictum metus.</p>
           </div>
 
-          <div class="col-xs-6 col-md-3">
+          <div className="col-xs-6 col-md-3">
             <h6>Categories</h6>
-            <ul class="footer-links">
+            <ul className="footer-links">
               <li><a href="http://scanfcode.com/category/c-language/">C</a></li>
               <li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>
               <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>
@@ -230,9 +230,9 @@ class LandingPage extends Component {
             </ul>
           </div>
 
-          <div class="col-xs-6 col-md-3">
+          <div className="col-xs-6 col-md-3">
             <h6>Quick Links</h6>
-            <ul class="footer-links">
+            <ul className="footer-links">
               <li><a href="http://scanfcode.com/about/">About Us</a></li>
               <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
               <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>
@@ -243,19 +243,19 @@ class LandingPage extends Component {
         </div>
         <hr />
       </div>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8 col-sm-6 col-xs-12">
-            <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 col-sm-6 col-xs-12">
+            <p className="copyright-text">Copyright &copy; 2017 All Rights Reserved
             </p>
           </div>
 
-          <div class="col-md-4 col-sm-6 col-xs-12">
-            <ul class="social-icons">
-              <li><a class="facebook" href="#"><FaSearch /></a></li>
-              <li><a class="twitter" href="#"><FaSearch /></a></li>
-              <li><a class="dribbble" href="#"><FaSearch /></a></li>
-              <li><a class="linkedin" href="#"><FaSearch /></a></li>   
+          <div className="col-md-4 col-sm-6 col-xs-12">
+            <ul className="social-icons">
+              <li><a className="facebook" href="#"><FaSearch /></a></li>
+              <li><a className="twitter" href="#"><FaSearch /></a></li>
+              <li><a className="dribbble" href="#"><FaSearch /></a></li>
+              <li><a className="linkedin" href="#"><FaSearch /></a></li>   
             </ul>
           </div>
         </div>
